97b22298d8fb81b8cb04ddb5b6fbf624fb41fe6b
Gilmar Trindade <43426254+ScarpelliniGilmar@users.noreply.github.com>
2019-04-08 20:21:37 -0300

    Atualização na pagina inicial
    Nesta atualização incluímos Cards e um carousel no topo da pagina inicial.

---
+++ Front-End/js/slidercard.js
@@ -0,0 +1,11 @@
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i
+    
