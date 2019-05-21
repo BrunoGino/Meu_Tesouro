54f1edbad4ee3931ee2f9abe88012982f7cbf670
Bruno Gino <bruno.gino24@gmail.com>
2019-05-21 18:04:06 -0300

    Corrigindo token
    Tokens corrigidos, na verdade parecia estar apenas expirando após um tempo estipulado. Agora é necessário login para entrar na tela de título, a mensagem de e-mail de confirmação foi atualizada e também a visualização dos cards. Próximo passo: Aplicação de filtro e botão de navegação (próximo e anterior) na janela de exibição de títulos.

--- meu_tesouro/website/views.py
+++ meu_tesouro/website/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
 from .models import Title
+from django.contrib.auth.decorators import login_required
 
-#Trata do tráfego de urls da aplicação web
+# Trata do tráfego de urls da aplicação web
 
 
 def home(request):
-    context = {
-        #'stocks': stock
-    }
-    return render(request, 'portal/index.html', {'title': 'Home'}, context)
+    return render(request, 'portal/index.html', {'title': 'Home'})
 
 
 def about(request):
     return render(request, 'portal/about.html', {'title': 'Sobre'})
 
-
+@login_required
 def titulos(request):
-    all_titles = Title.objects.all()
+    all_titles = Title.objects.all()[:20]
 
     context = {'all_titles': all_titles}
     return render(request, 'portal/cards.html', context, {'title': 'Títulos'})
-# Create your views here.
+
