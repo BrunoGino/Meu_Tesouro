63d2b3de5f6221abbd3f08066f11483ea4ba80e3
lexrebello <RZ7pJjHM646qQMw>
2019-06-13 23:31:09 -0300

    css refactor

---
+++ meu_tesouro/website/static/portal/js/script.js
@@ -0,0 +1,8 @@
+(function($) {
+    "use strict";
+
+    // manual carousel controls
+    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+
+})(jQuery);
